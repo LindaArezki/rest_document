1e36b23e983dde3d0a965f7fff2fda319b1cc65b
lindaarezki9 <lindaarezki9@gmail.com>
2023-04-06 19:07:36 +0200

    Mission 7 Bon paramètre de la BDD

--- AccessBDD.php
+++ AccessBDD.php
@@ -6,8 +6,8 @@ include_once("ConnexionPDO.php");
  */
 class AccessBDD {
 	
-    public $login="root";
-    public $mdp="";
+    public $login="Userbdd";
+    public $mdp="P@ssword0101!";
     public $bd="mediatek86";
     public $serveur="localhost";
     public $port="3306";	
